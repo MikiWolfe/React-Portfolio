1b5c90c1c2808cecf25bc11a3f2cbf22c865acd0
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-28 21:58:56 -0700

    push page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../styles/Header.css";
 export default function Header() {
   return (
     <div id="h1" className="gradient">
-    <img className="profile" src="assests/image/profile2.jpg" alt="Profile picture of my face" />
+    <img className="profile" src="./images/profile2.jpg" alt="Profile picture of my face" />
     <h1 className="example">Mikayla Bruce, LVT</h1>
   
     <h3 className="example">Full Stack Web Developer</h3>
