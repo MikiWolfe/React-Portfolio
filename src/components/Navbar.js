b888461858997c8852f343198ab427bd616f4752
Mikayla Bruce <mkla.w.b@gmail.com>
2022-04-04 18:01:12 -0700

    change to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,8 +25,9 @@ export default function Navbar({ currentPage, handlePageChange }) {
       >
         Contact Me
       </a>
-      <a className="link" href="https://drive.google.com/file/d/16le4MSng1pk_ynPzQJm7PfUAjGvk5Eb0/view?usp=sharing" target="_blank">
-        Resume
+      <a className="link" href="https://drive.google.com/file/d/16le4MSng1pk_ynPzQJm7PfUAjGvk5Eb0/view?usp=sharing" 
+      target="_blank">
+      Resume
         
       </a>
     </nav>
